240da3b83d50fb313471602592b180af2ec2fe82
SloMusti <musti@wlan-si.net>
2018-10-06 23:11:08 +0200

    Initial upload.

---
+++ HDC2080.h
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "Wire.h"
+
+#define ADDR 0x40
+
+class HDC2080{
+	public:
+		HDC2080();
+		void begin();
+		void read();
+		float getTemp();
+		float getHum();
+
+	private:
+		uint16_t temperatureRaw;
+		uint16_t humidityRaw;
+};
