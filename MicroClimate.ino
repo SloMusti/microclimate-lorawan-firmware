4d8988543bd2da08c7a355aea57f88ee68af0b19
musti <musti@wlan-si.net>
2018-10-07 12:41:29 +0200

    Implemented better LoraWAN config and timeout handling

--- MicroClimate.ino
+++ MicroClimate.ino
@@ -10,6 +10,7 @@
  *  TODO:
  *  Properly implement sensors for low-power operation
  *  Cover corner-case of Join failures etc...
+ *  
  *    
  *  This device has the following active I2C devices
  *  0x19  ! LIS2DH12
@@ -31,6 +32,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// TTN fair usage policy guideline
+// An average of 30 seconds uplink time on air, per day, per device. 
+// 20 messages per day at SF12, 500 messages per day at SF7, for 12byte payload
+// At most 10 downlink messages per day, including the ACKs for confirmed uplinks.
+
+
 #include "LoRaWAN.h"
 #include <STM32L0.h>
 #include <Wire.h> 
@@ -94,18 +102,6 @@ void setup( void )
       serial_debug.println(devEui); 
     #endif
 
-    //Configure lora parameters
-    LoRaWAN.begin(EU868);
-    LoRaWAN.addChannel(1, 868300000, 0, 6);
-    
-    LoRaWAN.setDutyCycle(true); // must be true except for development in confined environments
-    LoRaWAN.setAntennaGain(0.0); // mot be equal to the installed antenna
-    
-    LoRaWAN.joinOTAA(appEui, appKey, devEui);
-    LoRaWAN.setADR(true);
-    //LoRaWAN.setDataRate(1);
-    serial_debug.println("JOIN( )");
-
     // Each sensor should have a check to determine if present on boot
     // Code in reading should handle if sensor becomes unavailable
     // All sensors need to be placed in the low power mode
@@ -122,6 +118,24 @@ void setup( void )
     //hdc2080
     hdc2080.begin();
 
+    //Configure lora parameters
+    LoRaWAN.begin(EU868);
+    LoRaWAN.addChannel(1, 868300000, 0, 6);
+    
+    LoRaWAN.setDutyCycle(false); // must be true except for development in confined environments
+    LoRaWAN.setAntennaGain(0.0); // must be equal to the installed antenna
+    LoRaWAN.setADR(true);
+    LoRaWAN.setLinkCheckLimit(5); // number of uplinks link check is sent, 5 for experimenting, 20 otherwise
+    LoRaWAN.setLinkCheckDelay(2); // number of uplinks waiting for an answer, 2 for experimenting, 4 otherwise
+    LoRaWAN.setLinkCheckThreshold(1); // number of times link check fails to assert link failed, 1 for experimenting, 4 otherwise
+    // see examples/LoRaWAN_Disconnect/LoRaWAN_Disconnect.ino
+    //LoRaWAN.setDataRate(1);
+
+    LoRaWAN.joinOTAA(appEui, appKey, devEui);
+    serial_debug.println("JOIN( )"); // if join fails, retry will be in about 180s
+    //wait for 10s
+    delay(10000); // proceed to main loop
+
     
 }
 void loop( void )
@@ -132,37 +146,32 @@ void loop( void )
   if (!LoRaWAN.linkGateways())
   {
     LoRaWAN.rejoinOTAA();
+    #ifdef debug
+      serial_debug.println("rejoinOTAA");
+    #endif
   }
 
-  float stm32l0_vdd = STM32L0.getVDDA();
-  float stm32l0_temp = STM32L0.getTemperature();
-
-  //LIS
-  lis.read();      // get X Y and Z data at once
-  float lis_movement=lis.x+lis.y+lis.z;
-
-  //DPOS130 read
-  int32_t dsp310_temp;
-  int32_t dsp310_pres;
+  if (LoRaWAN.joined() && !LoRaWAN.busy())
+  {
+    float stm32l0_vdd = STM32L0.getVDDA();
+    float stm32l0_temp = STM32L0.getTemperature();
   
-  int ret = Dps310PressureSensor.measureTempOnce(dsp310_temp, 7);
-  ret += Dps310PressureSensor.measurePressureOnce(dsp310_pres, 7);
-  dsp310_pres=dsp310_pres/10;
-
-  hdc2080.read();
-  float hdc2080_temp = hdc2080.getTemp();
-  float hdc2080_hum = hdc2080.getHum();
+    //LIS
+    lis.read();      // get X Y and Z data at once
+    float lis_movement=lis.x+lis.y+lis.z;
   
-
-  // Build the lorawan message
-//  var I1 = bytes[0];
-//  var T1 = bytes[1];
-//  var T01 = bytes[2];
-//  var H1 = bytes[3];
-//  var H01 = bytes[4];
-//  var AP1 = (bytes[5] << 8) | bytes[6];
-//  var ACC1 = bytes[7];
-
+    //DPOS130 read
+    int32_t dsp310_temp;
+    int32_t dsp310_pres;
+    
+    int ret = Dps310PressureSensor.measureTempOnce(dsp310_temp, 7);
+    ret += Dps310PressureSensor.measurePressureOnce(dsp310_pres, 7);
+    dsp310_pres=dsp310_pres/10;
+  
+    hdc2080.read();
+    float hdc2080_temp = hdc2080.getTemp();
+    float hdc2080_hum = hdc2080.getHum();
+    
     #ifdef debug
       serial_debug.println(""); 
       serial_debug.print("stm32l0_vdd: "); 
@@ -179,73 +188,79 @@ void loop( void )
       serial_debug.println(hdc2080_temp);
       serial_debug.print("hdc2080_hum: "); 
       serial_debug.println(hdc2080_hum);  
+    #endif  
+   
+    #ifdef debug
+      serial_debug.print("TRANSMIT( ");
+      serial_debug.print("TimeOnAir: ");
+      serial_debug.print(LoRaWAN.getTimeOnAir());
+      serial_debug.print(", NextTxTime: ");
+      serial_debug.print(LoRaWAN.getNextTxTime());
+      serial_debug.print(", MaxPayloadSize: ");
+      serial_debug.print(LoRaWAN.getMaxPayloadSize());
+      serial_debug.print(", DR: ");
+      serial_debug.print(LoRaWAN.getDataRate());
+      serial_debug.print(", TxPower: ");
+      serial_debug.print(LoRaWAN.getTxPower(), 1);
+      serial_debug.print("dbm, UpLinkCounter: ");
+      serial_debug.print(LoRaWAN.getUpLinkCounter());
+      serial_debug.print(", DownLinkCounter: ");
+      serial_debug.print(LoRaWAN.getDownLinkCounter());
+      serial_debug.println(" )");
     #endif
+
+
+    LoRaWAN.beginPacket();
+    /* I1 - STATUS HEADER BYTE
+    bit7 - error
+    bit6 - alarm/interrupt
+    bit5 - reserved 
+    bit4 - reserved
+    bit3 - ADCs
+    bit2 - preassure
+    bit1 - temperature+humidity
+    bit0 - accelerometer
+    */
+    LoRaWAN.write(0x01);//I1
+
+    //BASIC DATA
+    int8_t t1 = (int8_t)hdc2080_temp;
+    uint8_t t01 = (hdc2080_temp-t1)*100;
+    LoRaWAN.write(t1);//T1
+    LoRaWAN.write(t01);//T01
+    int8_t h1 = (int8_t)hdc2080_hum;
+    uint8_t h01 = (hdc2080_hum-h1)*100;
+    LoRaWAN.write(h1);//H1
+    LoRaWAN.write(h01);//H01
+    uint16_t ap = (uint16_t)dsp310_pres;
+    LoRaWAN.write(highByte(ap));//AP1 high byte
+    LoRaWAN.write(lowByte(ap));//AP2 low byte
+    LoRaWAN.write((int8_t)lis_movement/1000);//ACC
     
-    if (LoRaWAN.joined() && !LoRaWAN.busy())
-    {
-      #ifdef debug
-        serial_debug.print("TRANSMIT( ");
-        serial_debug.print("TimeOnAir: ");
-        serial_debug.print(LoRaWAN.getTimeOnAir());
-        serial_debug.print(", NextTxTime: ");
-        serial_debug.print(LoRaWAN.getNextTxTime());
-        serial_debug.print(", MaxPayloadSize: ");
-        serial_debug.print(LoRaWAN.getMaxPayloadSize());
-        serial_debug.print(", DR: ");
-        serial_debug.print(LoRaWAN.getDataRate());
-        serial_debug.print(", TxPower: ");
-        serial_debug.print(LoRaWAN.getTxPower(), 1);
-        serial_debug.print("dbm, UpLinkCounter: ");
-        serial_debug.print(LoRaWAN.getUpLinkCounter());
-        serial_debug.print(", DownLinkCounter: ");
-        serial_debug.print(LoRaWAN.getDownLinkCounter());
-        serial_debug.println(" )");
-      #endif
-
-
-        LoRaWAN.beginPacket();
-        /* I1 - STATUS HEADER BYTE
-        bit7 - error
-        bit6 - alarm/interrupt
-        bit5 - reserved 
-        bit4 - reserved
-        bit3 - ADCs
-        bit2 - preassure
-        bit1 - temperature+humidity
-        bit0 - accelerometer
-        */
-        LoRaWAN.write(0x01);//I1
-
-        //BASIC DATA
-        int8_t t1 = (int8_t)hdc2080_temp;
-        uint8_t t01 = (hdc2080_temp-t1)*100;
-        LoRaWAN.write(t1);//T1
-        LoRaWAN.write(t01);//T01
-        int8_t h1 = (int8_t)hdc2080_hum;
-        uint8_t h01 = (hdc2080_hum-h1)*100;
-        LoRaWAN.write(h1);//H1
-        LoRaWAN.write(h01);//H01
-        uint16_t ap = (uint16_t)dsp310_pres;
-        LoRaWAN.write(highByte(ap));//AP1 high byte
-        LoRaWAN.write(lowByte(ap));//AP2 low byte
-        LoRaWAN.write((int8_t)lis_movement/1000);//ACC
-        
-        //EXTENDED DATA
-        uint16_t vdd = stm32l0_vdd*100;
-        LoRaWAN.write(highByte(vdd));//VBAT
-        LoRaWAN.write(lowByte(vdd));//VBAT
-        int8_t tc1 = (int8_t)stm32l0_temp;
-        uint8_t tc01 = (stm32l0_temp-tc1)*100;
-        LoRaWAN.write(tc1);//TCPU1
-        LoRaWAN.write(tc01);//TCPU01
-        
-        LoRaWAN.endPacket();
-    }
-    //delay(10000);
-
+    //EXTENDED DATA
+    uint16_t vdd = stm32l0_vdd*100;
+    LoRaWAN.write(highByte(vdd));//VBAT
+    LoRaWAN.write(lowByte(vdd));//VBAT
+    int8_t tc1 = (int8_t)stm32l0_temp;
+    uint8_t tc01 = (stm32l0_temp-tc1)*100;
+    LoRaWAN.write(tc1);//TCPU1
+    LoRaWAN.write(tc01);//TCPU01
     
+    LoRaWAN.endPacket();
+
+    low_power_sleep(60*1000);//send data every 10s    
+  }
+  else{
+    low_power_sleep(180*1000);//retry join in 180s  
+  }
+}
+
+void low_power_sleep(long duration){
   //serial_debug.end();
   //USBDevice.detach();
-  serial_debug.println("sleep for 60s");
-  STM32L0.stop(60000);
+  #ifdef debug
+    serial_debug.print("sleep for ");
+    serial_debug.println(duration);
+  #endif
+  STM32L0.stop(duration);
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # microclimate-lorawan-firmware
-Firmware for microclimate.network device in Arduino environment, under development, use not recommended.
+Unofficial firmware for microclimate.network device in Arduino environment, under development, use not recommended. See microclimate.network for official content.
+
+# LoraWAN configuration
+OTAA activation with ADR and auto-rejoin upon link failures. First join after boot retry is ever 180s.
 
 # Known issues
 Not optimized for very low power operation yet.
